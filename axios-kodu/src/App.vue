a547b188a923029e42829903279c8ee46dc93c81
blacke4gle <ahmetdemirr0@gmail.com>
2022-05-23 17:54:12 -0700

    Axios
    Axios kullanımı ile verilerin gönderilip, alınması ve  ekranda gösterilmesi

---
+++ axios-kodu/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-fluid p-0">
+    <app-header></app-header>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+<script>
+import Header from "./components/Header";
+export default {
+  components: {
+    appHeader: Header,
+  },
+};
+</script>
+<style>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+  opacity: 1;
+}
+</style>
